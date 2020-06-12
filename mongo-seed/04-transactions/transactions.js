8b6942199e91d05bdd2210f3bdbb15e2c2a6a1b4
Monica Damasceno <monicadamasceno@gmail.com>
2020-06-12 19:36:29 -0300

    create fake data of transactions for test

---
+++ mongo-seed/04-transactions/transactions.js
@@ -0,0 +1,41 @@
+const { getObjectId } = require("mongo-seeding");
+
+const transactions = [
+  {
+    payer: getObjectId("account1"),
+    beneficiary: getObjectId("account3"),
+    type: "transfer",
+    value: 200,
+    date: new Date("2010-05-20T13:34:22"),
+  },
+  {
+    payer: getObjectId("account1"),
+    companyName: "Company1",
+    type: "payment",
+    value: 200,
+    date: new Date("2010-05-28T09:24:33"),
+  },
+  {
+    payer: getObjectId("account2"),
+    beneficiary: getObjectId("account1"),
+    type: "transfer",
+    value: 20,
+    date: new Date("2010-05-20T20:34:45"),
+  },
+  {
+    payer: getObjectId("account2"),
+    beneficiary: getObjectId("account1"),
+    type: "transfer",
+    value: 20,
+    date: new Date("2010-05-20T20:34:45"),
+  },
+  {
+    payer: getObjectId("account2"),
+    companyName: "Company2",
+    type: "payment",
+    value: 230,
+    date: new Date("2010-05-28T09:24:33"),
+  },
+];
+
+module.exports = transactions;
