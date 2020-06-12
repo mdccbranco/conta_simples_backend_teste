233ff98eab44be2ef5c9f47036fcd75ab6450289
Monica Damasceno <monicadamasceno@gmail.com>
2020-06-12 19:36:03 -0300

    add '_id' on accounts data

--- mongo-seed/03-accounts/accounts.js
+++ mongo-seed/03-accounts/accounts.js
@@ -5,16 +5,19 @@ const accounts = [
     owner: getObjectId("user1"),
     bank: getObjectId("bank1"),
     accountNumber: 121,
+    _id: getObjectId("account1"),
   },
   {
     owner: getObjectId("user2"),
     bank: getObjectId("bank1"),
     accountNumber: 122,
+    _id: getObjectId("account2"),
   },
   {
     owner: getObjectId("user1"),
     bank: getObjectId("bank2"),
     accountNumber: 123,
+    _id: getObjectId("account3"),
   },
 ];
 
