e979ed9ef7de0b483d308e1806ba8e8d689fc577
Monica Damasceno <monicadamasceno@gmail.com>
2020-06-13 12:39:08 -0300

    add a new route to get users account and modify transaction route to get all transactions from an account

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.post("/login", async (req, res) => {
     const db = mongo.getDb();
     const user = await db.collection("users").findOne({ username, password });
     const token = jwt.sign({ username: user.username, _id: user._id }, JWT_PW);
-    res.status(200).json({ token });
+    res.status(200).json({ token, userID: user._id });
   } catch (err) {
     res.status(500).json({ msg: "Failed to authenticate" });
   }
@@ -50,102 +50,148 @@ app.use((req, res, next) => {
   }
 });
 
-app.get("/transactions/:id", async (req, res) => {
-  const { id } = req.params;
-  if(req.user._id === id){
-  try {
-    const db = mongo.getDb();
-    const transactons = await db
-      .collection("transactions")
-      .aggregate([
-        {
-          $lookup: {
-            from: "accounts",
-            localField: "payer",
-            foreignField: "_id",
-            as: "payer",
+app.get("/accounts/:userId", async (req, res) => {
+  const { userId } = req.params;
+  if (req.user._id === userId) {
+    try {
+      const db = mongo.getDb();
+      const accounts = await db
+        .collection("accounts")
+        .aggregate([
+          {
+            $lookup: {
+              from: "banks",
+              localField: "bank",
+              foreignField: "_id",
+              as: "bank",
+            },
           },
-        },
-        {
-          $lookup: {
-            from: "accounts",
-            localField: "beneficiary",
-            foreignField: "_id",
-            as: "beneficiary",
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+            },
           },
-        },
-        { $unwind: { path: "$beneficiary", preserveNullAndEmptyArrays: true } },
-        { $unwind: "$payer" },
-        {
-          $lookup: {
-            from: "users",
-            localField: "payer.owner",
-            foreignField: "_id",
-            as: "payer.owner",
+          { $unwind: "$owner" },
+          { $unwind: "$bank" },
+          { $project: { "owner.password": 0 } },
+          {
+            $match: {
+              "owner._id": ObjectId(userId),
+            },
           },
-        },
-        {
-          $lookup: {
-            from: "banks",
-            localField: "payer.bank",
-            foreignField: "_id",
-            as: "payer.bank",
-          },
-        },
-        { $unwind: "$payer.owner" },
-        { $unwind: "$payer.bank" },
-        {
-          $lookup: {
-            from: "users",
-            localField: "beneficiary.owner",
-            foreignField: "_id",
-            as: "beneficiary.owner",
-          },
-        },
-        {
-          $lookup: {
-            from: "banks",
-            localField: "beneficiary.bank",
-            foreignField: "_id",
-            as: "beneficiary.bank",
-          },
-        },
-        {
-          $unwind: {
-            path: "$beneficiary.owner",
-            preserveNullAndEmptyArrays: true,
-          },
-        },
-        {
-          $unwind: {
-            path: "$beneficiary.bank",
-            preserveNullAndEmptyArrays: true,
-          },
-        },
-        {
-          $project: {
-            "payer.owner.password": 0,
-            "beneficiary.owner.password": 0,
-          },
-        },
-        {
-          $match: {
-            $or: [
-              { "payer.owner._id": ObjectId(id) },
-              { "beneficiary.owner._id": ObjectId(id) },
-            ],
-          },
-        },
-      ])
-      .toArray();
+        ])
+        .toArray();
 
-    res.status(200).json(transactons);
-  } catch (err) {
-  res.status(500).json({ msg: "Failed to get data" });
+      res.status(200).json(accounts);
+    } catch (err) {
+      res.status(500).json({ msg: "Failed to get data" });
+    }
+  } else {
+    res.status(500).json({ msg: "Unauthorized user" });
   }
-} else {
-  res.status(500).json({ msg: "Unauthorized user" });
-}
+});
+
+app.get("/transactions/:userId/:accountId", async (req, res) => {
+  const { userId, accountId } = req.params;
+  if (req.user._id === userId) {
+    try {
+      const db = mongo.getDb();
+      const transactons = await db
+        .collection("transactions")
+        .aggregate([
+          {
+            $lookup: {
+              from: "accounts",
+              localField: "payer",
+              foreignField: "_id",
+              as: "payer",
+            },
+          },
+          {
+            $lookup: {
+              from: "accounts",
+              localField: "beneficiary",
+              foreignField: "_id",
+              as: "beneficiary",
+            },
+          },
+          {
+            $unwind: { path: "$beneficiary", preserveNullAndEmptyArrays: true },
+          },
+          { $unwind: "$payer" },
+          {
+            $lookup: {
+              from: "users",
+              localField: "payer.owner",
+              foreignField: "_id",
+              as: "payer.owner",
+            },
+          },
+          {
+            $lookup: {
+              from: "banks",
+              localField: "payer.bank",
+              foreignField: "_id",
+              as: "payer.bank",
+            },
+          },
+          { $unwind: "$payer.owner" },
+          { $unwind: "$payer.bank" },
+          {
+            $lookup: {
+              from: "users",
+              localField: "beneficiary.owner",
+              foreignField: "_id",
+              as: "beneficiary.owner",
+            },
+          },
+          {
+            $lookup: {
+              from: "banks",
+              localField: "beneficiary.bank",
+              foreignField: "_id",
+              as: "beneficiary.bank",
+            },
+          },
+          {
+            $unwind: {
+              path: "$beneficiary.owner",
+              preserveNullAndEmptyArrays: true,
+            },
+          },
+          {
+            $unwind: {
+              path: "$beneficiary.bank",
+              preserveNullAndEmptyArrays: true,
+            },
+          },
+          {
+            $project: {
+              "payer.owner.password": 0,
+              "beneficiary.owner.password": 0,
+            },
+          },
+          {
+            $match: {
+              $or: [
+                { "payer._id": ObjectId(accountId) },
+                { "beneficiary._id": ObjectId(accountId) },
+              ],
+            },
+          },
+        ])
+        .toArray();
+
+      res.status(200).json(transactons);
+    } catch (err) {
+      res.status(500).json({ msg: "Failed to get data" });
+    }
+  } else {
+    res.status(500).json({ msg: "Unauthorized user" });
+  }
 });
 
 app.listen(PORT !== undefined ? PORT : 3300, () => {
